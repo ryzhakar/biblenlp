e52f2547548f1e70579c91a51e2468f4f0ba6394
Arthur Ryzhak <ryzhakar@gmail.com>
2022-07-07 12:28:34 +0300

    Add verse breakdown
    Unify structure into str-based dicts

--- untangle_xml.py
+++ untangle_xml.py
@@ -1,7 +1,16 @@
+"""This script targets building a json structure book-chapter-verse-originalwords.
+Inner verse structure besides the original words is not discarded, but information is somewhat preserved.
+Any other information is explicitly filtered out.
+
+Some of the functions below are reusable.
+"""
+
 import json
-from typing import Any, Sequence
+import re
+from typing import Any, Mapping, Sequence, Union
 
 
+# File management
 def to_json(filename, data):
     with open(filename, 'w+') as f:
         json.dump(data, f, indent=2, ensure_ascii=False)
@@ -10,47 +19,115 @@ def load_lines(filename):
     with open(filename, 'r') as f:
         return f.read().split('\n')
 
-def write_lines(filename, lines):
-    with open(filename, 'w+') as f:
-        f.write('\n'.join(lines))
 
+# Filtering
+def starts_with_tags(line: str, tags: Sequence[str]) -> bool:
 
+    if len(tags) > 1:
+        tags = '|'.join(tags)
+    else:
+        tags = tags[0]
 
-def starts_with_tag(line: str, tags: Sequence[str]) -> bool:
+    return re.match(fr'\s*</*({tags})', line) is not None
 
-    open = [
-        line.startswith(f'<{tg}') or line.startswith(f'</{tg}')
-        for tg in tags
+def except_stuff(line: str):
+    stuff = [
+        '<div type="colophon"'
     ]
-    return any(open)
+    return any(line.startswith(x) for x in stuff)
 
-def filter_lines(tags: Sequence[str], lines: Sequence[str], filter_func):
+
+def specific_filtering(line: str, tags: Sequence[str]) -> bool:
+    """Combines starts_with_tags and except_stuff"""
+
+    return starts_with_tags(line, tags) and not except_stuff(line)
+
+def filter_lines(
+    tags: Sequence[str],
+    lines: Sequence[str],
+    filter_method
+    ) -> Sequence[str]:
     """Leaves only lines that start with specified tags"""
     
-    return [line for line in lines if filter_func(line, tags)]
+    return [line for line in lines if filter_method(line, tags)]
 
-def build_structure(tags: Sequence[str], lines: Sequence[str]) -> Sequence[Any]:
+
+# Parsing in place
+def separate_verse(line: str) -> Sequence[str]:
+    """Returns a list of <verse> tags and their contents"""
+    return [
+        x
+        for x in re.split(r'(<verse osisID.+?/>)(.+?)(<verse eID.+?/>)', line)
+        if x
+        ]
+
+def separate_original_words(line: str) -> Sequence[Sequence[str]]:
+    """Returns a tuple of lists of original words and of everything else"""
+    words = re.findall(r'<w.+?/*>.+?</w>', line)
+    stuff = re.split(r'<w.+?/*>.+?</w>', line)
+
+    return (words, stuff)
+
+def parse_verses(lines: Sequence[str]) -> Sequence[dict]:
+    """Parses a verse into a dict"""
+    w_sep_tags = [separate_verse(line) for line in lines]
+    named_array_words = [
+        {tg[0]: separate_original_words(tg[1])}
+        for tg in w_sep_tags
+    ]
+    return named_array_words
+
+
+
+
+# Structuring
+def build_raw_structure(
+    tags: Sequence[str],
+    lines: Sequence[str],
+    deepest_level_method,
+    ) -> Sequence[Any]:
     """Constructs a list of dicts with tag lines as keys and other lines as values"""
     layer = []
     tagline = ''
     if not tags:
-        return lines
+        return deepest_level_method(lines)
     
     for line in lines:
         if line.lstrip().startswith(f'<{tags[0]}'):
             tagline = line
             layer.append({tagline: list()})
         elif line.lstrip().startswith(f'</{tags[0]}'):
-            layer[-1][tagline] = build_structure(tags[1:], layer[-1][tagline])
+            layer[-1][tagline] = build_raw_structure(tags[1:], layer[-1][tagline], deepest_level_method)
         else:
             layer[-1][tagline].append(line)
     return layer
 
+def unify_structure(structure: Sequence[dict]) -> Union[Mapping, Sequence]:
+    """Unifies the structure of a list of dicts"""
+    
+    
+    if isinstance(structure[0], dict):
+        unified = dict()
+        for d in structure:
+            key = list(d.keys())[0]
+            value = d[key]
+            unified[key] = unify_structure(value)
+        return unified
+    else:
+        return structure
+
+
+
+# Main function
+def untangle_osis(file_wo_ext: str):
+    lines = load_lines(f'{file_wo_ext}.xml')
+    lines = filter_lines(['div', 'chapter', 'verse'], lines, specific_filtering)
+    layers = build_raw_structure(['div', 'chapter'], lines, parse_verses)
+    layers = unify_structure(layers)
+    to_json(f'{file_wo_ext}.json', layers)
 
 
 
 if __name__ == "__main__":
-    lines = load_lines('kjv.xml')
-    lines = filter_lines(lines, ['div', 'chapter', 'verse'], starts_with_tag)
-    layers = build_structure(['div', 'chapter'], lines)
-    to_json('kjv.json', layers)
+    file_wo_ext = input('Enter the file name without extension: ')
+    untangle_osis(file_wo_ext)
