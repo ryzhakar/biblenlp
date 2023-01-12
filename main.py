1edf82a068826b12785e5380929c2b437b3bc805
Arthur Ryzhak <ryzhakar@gmail.com>
2023-01-13 00:30:59 +0200

    Write json in main.py

--- main.py
+++ main.py
@@ -6,3 +6,6 @@ if __name__ == "__main__":
     from devtools import debug
     raw_bible = untangle_osis("kjvfull.xml")
     pybible = parse_bible(raw_bible)  # type: ignore
+    
+    with open('.bib.json', 'w+') as f:
+        f.write(pybible.json(indent=0, exclude_unset=True))
