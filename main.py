27831a9d2598b9e3600332c0b6a897e957b0bd00
Arthur Ryzhak <ryzhakar@gmail.com>
2023-01-13 23:35:40 +0200

    chore: add a proper wemake config

--- main.py
+++ main.py
@@ -2,10 +2,8 @@ from biblenlp.restructuring import parse_from_xml_str_bible as parse_bible
 from biblenlp.restructuring import untangle_osis
 
 
-if __name__ == '__main__':
-    from devtools import debug
-    raw_bible = untangle_osis('kjvfull.xml')
-    pybible = parse_bible(raw_bible)  # type: ignore
+raw_bible = untangle_osis('kjvfull.xml')
+pybible = parse_bible(raw_bible)  # type: ignore
 
-    with open('.bib.json', 'w+') as f:
-        f.write(pybible.json(indent=0, exclude_unset=True))
+with open('.bib.json', 'w+') as json_file:
+    json_file.write(pybible.json(indent=0, exclude_unset=True))
